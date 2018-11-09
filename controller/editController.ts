ddaaadfd11f6ae2b50b23185e168d771c540d575
Pascal Knecht <pascal.knecht@hsr.ch>
2018-11-09 11:07:32 +0100

    remove duplicated code

--- controller/editController.ts
+++ controller/editController.ts
@@ -6,6 +6,18 @@ import noteStore from "../model/noteStore";
 
 const debug: (msg: string) => void = require('debug')('EditController');
 
+function renderContent(req : Request, res : Response, note : Note) {
+    res.render('edit', {
+        _id: req.params._id,
+        title: 'Note Pro - Edit',
+        styleName: Style[req.session.style],
+        screenreader: req.session.screenreader,
+        note: note,
+        DEBUG1: JSON.stringify(req.session),
+        DEBUG2: JSON.stringify(req.body)
+    } as EditOptions);
+}
+
 export function editController(req: Request, res: Response, next: NextFunction) {
   if (req.body.cancel) {
     res.redirect('/');
@@ -22,32 +34,14 @@ export function editController(req: Request, res: Response, next: NextFunction) 
       debug('Validation of note failed!');
 
       // validate failed.. re-render
-      res.render('edit', {
-        _id: req.params._id,
-        title: 'Note Pro - Edit',
-        styleName: Style[req.session.style],
-        screenreader: req.session.screenreader,
-        note: note,
-        error: new Error(errors.toString()),
-        DEBUG1: JSON.stringify(req.session),
-        DEBUG2: JSON.stringify(req.body)
-      } as EditOptions);
+        renderContent(req, res, note);
       return;
     }
 
     noteStore.update(note, (err: Error, noteResponse: Note) => {
       if (err) {
         // save failed.. re-render
-        res.render('edit', {
-          _id: req.params._id,
-          title: 'Note Pro - Edit',
-          styleName: Style[req.session.style],
-          screenreader: req.session.screenreader,
-          note: note,
-          error: err,
-          DEBUG1: JSON.stringify(req.session),
-          DEBUG2: JSON.stringify(req.body)
-        } as EditOptions);
+          renderContent(req, res, note);
       } else {
         res.redirect('/');
       }
@@ -59,28 +53,13 @@ export function editController(req: Request, res: Response, next: NextFunction) 
         if (err) {
           next(err);
         } else {
-          res.render('edit', {
-            _id: req.params._id,
-            title: 'Note Pro - Edit',
-            styleName: Style[req.session.style],
-            screenreader: req.session.screenreader,
-            note: note,
-            DEBUG1: JSON.stringify(req.session),
-            DEBUG2: JSON.stringify(req.body)
-          } as EditOptions);
+            renderContent(req, res, note);
         }
       });
       return;
     }
   }
 
-  res.render('edit', {
-    title: 'Note Pro - Add',
-    styleName: Style[req.session.style],
-    screenreader: req.session.screenreader,
-    note: {},
-    DEBUG1: JSON.stringify(req.session),
-    DEBUG2: JSON.stringify(req.body)
-  } as EditOptions);
+  renderContent(req, res, null);
 }
 export default editController;
