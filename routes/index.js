8fb5a0bbfbe7841019fbb6b795f6f17a1d994aef
Markus Gachnang <markusgachnang@gmx.ch>
2018-11-09 10:42:50 +0100

    Apply gitignore -> deletion of generated files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const indexController_1 = require("../controller/indexController");
-exports.router = express.Router();
-exports.default = exports.router;
-/* GET home page. */
-exports.router.all('/', indexController_1.default);
-//# sourceMappingURL=index.js.map
